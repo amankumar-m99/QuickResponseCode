8ea022faae1b802e6056dbd4b69f70bc8f06475c
Aman Kumar <akumar10350@truechip.net>
2023-10-09 17:03:04 +0530

    added option to save image of qr

--- src/app/generator/generator.component.ts
+++ src/app/generator/generator.component.ts
@@ -1,20 +1,36 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+import * as htmlToImage from 'html-to-image';
+
 @Component({
   selector: 'app-generator',
   templateUrl: './generator.component.html',
   styleUrls: ['./generator.component.css']
 })
 export class GeneratorComponent {
-  public qrData:string="";
-  public isQrGenerated=false;
-  constructor(){}
+  public qrData: string = "";
+  public isQrGenerated = false;
+  constructor() { }
 
-  submit(qrFormData:NgForm){
-    console.log(qrFormData.value);
+  submit(qrFormData: NgForm) {
     this.qrData = qrFormData.value["qrdata"];
-    console.log(this.qrData);
     this.isQrGenerated = true;
+  }
+
+  downloadQR() {
+    let node = document.getElementById('generated-qr-code');
+    if (node != null) {
+      htmlToImage.toJpeg(node, { quality: 0.95 })
+        .then(function (dataUrl) {
+          var link = document.createElement('a');
+          link.download = 'quick-response-code.jpeg';
+          link.href = dataUrl;
+          link.click();
+        });
+    }
+    else{
+      alert("No qr genrated");
+    }
   }
 }
